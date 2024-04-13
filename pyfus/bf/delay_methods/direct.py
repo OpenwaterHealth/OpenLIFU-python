c598c3bef0483c3c8925d38ae833670fa6454947
Peter Hollender <peterhollender@gmail.com>
2024-04-12 23:37:21 -0400

    Add c0 parameter to DirectDelays to match json

--- pyfus/bf/delay_methods/direct.py
+++ pyfus/bf/delay_methods/direct.py
@@ -3,12 +3,16 @@ import xarray as xa
 import numpy as np
 from pyfus import Transducer, Point
 from pyfus.bf.delay_methods import DelayMethod
-from typing import ClassVar
+from typing import ClassVar, Optional
 
 @dataclass
 class Direct(DelayMethod):
-    def calc_delays(self, arr: Transducer, target: Point, params: xa.Dataset, transform: bool = True):
-        c = params['sound_speed'].attrs['ref_value']
+    c0: float = 1480.0
+    def calc_delays(self, arr: Transducer, target: Point, params: Optional[xa.Dataset]=None, transform: bool = True):
+        if params is None:
+            c = self.c0
+        else:
+            c = params['sound_speed'].attrs['ref_value']
         target_pos = target.get_position(units="m")
         matrix = arr.get_matrix(units="m") if transform else np.eye(4)
         dists = np.array([el.distance_to_point(target_pos, units="m", matrix=matrix) for el in arr.elements])
