6ba79eda53635e87754ed59b3f418fd5bb2aba2d
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2024-08-21 15:52:05 -0400

    Support serialization for Protocols (#85)

---
+++ tests/test_seg_method.py
@@ -0,0 +1,36 @@
+from openlifu.seg import SegmentationMethod, Material
+from pathlib import Path
+import pytest
+
+@pytest.fixture()
+def example_seg_method() -> SegmentationMethod:
+    return SegmentationMethod(
+        materials = {
+            'water' : Material(
+                id="water",
+                name="water",
+                sound_speed=1500.0,
+                density=1000.0,
+                attenuation=0.0,
+                specific_heat=4182.0,
+                thermal_conductivity=0.598
+            ),
+            'skull' : Material(
+                id="skull",
+                name="skull",
+                sound_speed=4080.0,
+                density=1900.0,
+                attenuation=0.0,
+                specific_heat=1100.0,
+                thermal_conductivity=0.3
+            ),
+        },
+        ref_material = 'water',
+    )
+
+def test_seg_method_dict_conversion(example_seg_method : SegmentationMethod):
+    assert SegmentationMethod.from_dict(example_seg_method.to_dict()) == example_seg_method
+
+def test_seg_method_dict_conversion_with_varied_param_ids(example_seg_method : SegmentationMethod):
+    example_seg_method.materials['water'].param_ids = ("something", "else") # change param ids to something else
+    assert SegmentationMethod.from_dict(example_seg_method.to_dict()) == example_seg_method # verify that the dict conversion still works.
