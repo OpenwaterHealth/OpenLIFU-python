f11e3537ad5e10857191b670ffe294bafece45ed
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2024-09-19 16:24:41 -0400

    Add unit test for Sequence
    This is part of work towards #103, since Solution needs to ensure
    that Sequence works. Unit testing Sequence by itself makes sense.

---
+++ tests/test_sequence.py
@@ -0,0 +1,10 @@
+from helpers import dataclasses_are_equal
+
+from openlifu import Sequence
+
+
+def test_dict_undict_sequence():
+    """Test that conversion between Sequence and dict works"""
+    sequence = Sequence(pulse_interval=2, pulse_count=5, pulse_train_interval=1.5, pulse_train_count=3)
+    reconstructed_sequence = Sequence.from_dict(sequence.to_dict())
+    assert dataclasses_are_equal(sequence, reconstructed_sequence)
