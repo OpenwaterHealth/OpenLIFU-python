17a6983465b6021adca059ef53839d609aa43c56
Loic Tetrel <loic.tetrel@kitware.com>
2024-10-16 17:08:05 +0200

    Port over minimal solution analysis code from matlab (#104)
    This adds minimal solution analysis code so it outputs at least the `mainlobe_pnp_MPa` attribute. The rest has been commented out for future work, additionnal modules/functions are also added to prepare future work.
    
    Update src/openlifu/bf/calc_dist_from_focus.py
    
    Fix circular import issue
    
    Co-authored-by: Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
    
    pre-commit hook
    
    Fix analyze function spelling
    
    units param is not needed in `calc_dist_from_focus`
    
    bad arguments for `offset_grid` and pylint false positive on imports
    
    fixed input xarray type hints for several functions
    
    distance units now available in SolutionOptions
    
    Removed `get_focus_matrix` because it is already implemented in geo.Point
    
    Adding missing unit tests for solution analysis
    
    Removed `calc_dist_from_focus`, the algorithm is now in `mask_focus`
    
    pre-commit hook

---
+++ tests/test_offset_grid.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pytest
+from xarray import DataArray, Dataset
+
+from openlifu.bf import offset_grid
+from openlifu.geo import Point
+
+
+@pytest.fixture()
+def example_focus() -> DataArray:
+    return Point(
+        id="test_focus_point",
+        radius=1.,
+        position=np.array([0.5, 0.5, 0.5]),
+        dims=("x", "y", "z"),
+        units="mm"
+    )
+
+@pytest.fixture()
+def example_xarr() -> DataArray:
+    rng = np.random.default_rng(147)
+    return Dataset(
+            {
+                'p': DataArray(
+                    data=rng.random((3, 2, 3)),
+                    dims=["x", "y", "z"],
+                    attrs={'units': "Pa"}
+                )
+            },
+            coords={
+                'x': DataArray(dims=["x"], data=np.linspace(0, 1, 3), attrs={'units': "mm"}),
+                'y': DataArray(dims=["y"], data=np.linspace(0, 1, 2), attrs={'units': "mm"}),
+                'z': DataArray(dims=["z"], data=np.linspace(0, 1, 3), attrs={'units': "mm"})
+            }
+        )
+
+def test_offset_grid(example_xarr: Dataset, example_focus: Point):
+    """Test that the distance grid from the focus point is correct."""
+    expected = np.array([
+       [[[ 0.00000000e+00,  0.00000000e+00,  0.00000000e+00],
+         [-3.53553391e-01, -2.04124145e-01,  2.88675135e-01],
+         [-7.07106781e-01, -4.08248290e-01,  5.77350269e-01]],
+
+        [[ 0.00000000e+00,  8.16496581e-01,  5.77350269e-01],
+         [-3.53553391e-01,  6.12372436e-01,  8.66025404e-01],
+         [-7.07106781e-01,  4.08248290e-01,  1.15470054e+00]]],
+
+
+       [[[ 3.53553391e-01, -2.04124145e-01,  2.88675135e-01],
+         [ 0.00000000e+00, -4.08248290e-01,  5.77350269e-01],
+         [-3.53553391e-01, -6.12372436e-01,  8.66025404e-01]],
+
+        [[ 3.53553391e-01,  6.12372436e-01,  8.66025404e-01],
+         [ 0.00000000e+00,  4.08248290e-01,  1.15470054e+00],
+         [-3.53553391e-01,  2.04124145e-01,  1.44337567e+00]]],
+
+
+       [[[ 7.07106781e-01, -4.08248290e-01,  5.77350269e-01],
+         [ 3.53553391e-01, -6.12372436e-01,  8.66025404e-01],
+         [ 0.00000000e+00, -8.16496581e-01,  1.15470054e+00]],
+
+        [[ 7.07106781e-01,  4.08248290e-01,  1.15470054e+00],
+         [ 3.53553391e-01,  2.04124145e-01,  1.44337567e+00],
+         [ 0.00000000e+00,  0.00000000e+00,  1.73205081e+00]]]])
+    offset = offset_grid(example_xarr, example_focus)
+
+    np.testing.assert_almost_equal(offset, expected)
