945a7a9d60f03b7cb0dae345e76cea5c8d6d7f82
Loic Tetrel <loic.tetrel@kitware.com>
2024-10-16 17:08:05 +0200

    Port over minimal solution analysis code from matlab (#104)
    This adds minimal solution analysis code so it outputs at least the `mainlobe_pnp_MPa` attribute. The rest has been commented out for future work, additionnal modules/functions are also added to prepare future work.
    
    Update src/openlifu/bf/calc_dist_from_focus.py
    
    Fix circular import issue
    
    Co-authored-by: Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
    
    pre-commit hook
    
    Fix analyze function spelling
    
    units param is not needed in `calc_dist_from_focus`
    
    bad arguments for `offset_grid` and pylint false positive on imports
    
    fixed input xarray type hints for several functions
    
    distance units now available in SolutionOptions
    
    Removed `get_focus_matrix` because it is already implemented in geo.Point
    
    Adding missing unit tests for solution analysis
    
    Removed `calc_dist_from_focus`, the algorithm is now in `mask_focus`
    
    pre-commit hook

--- src/openlifu/bf/__init__.py
+++ src/openlifu/bf/__init__.py
@@ -1,6 +1,9 @@
 from .apod_methods import ApodizationMethod
 from .delay_methods import DelayMethod
 from .focal_patterns import FocalPattern, SinglePoint, Wheel
+from .get_beamwidth import get_beamwidth
+from .mask_focus import mask_focus
+from .offset_grid import offset_grid
 from .pulse import Pulse
 from .sequence import Sequence
 
@@ -12,4 +15,7 @@ __all__ = [
     "SinglePoint",
     "Pulse",
     "Sequence",
+    "offset_grid",
+    "mask_focus",
+    "get_beamwidth"
 ]
--- tests/test_solution.py
+++ tests/test_solution.py
@@ -3,10 +3,27 @@ from pathlib import Path
 
 import numpy as np
 import pytest
-import xarray
+import xarray as xa
 from helpers import dataclasses_are_equal
 
-from openlifu import Point, Pulse, Sequence, Solution
+from openlifu import Point, Pulse, Sequence, Solution, Transducer
+from openlifu.xdc.element import Element
+
+
+@pytest.fixture()
+def example_transducer() -> Transducer:
+    return Transducer(
+        id="trans_456",
+        name="Test Transducer",
+        elements=[
+            Element(index=1, x=-14, y=-14, units="m"),
+            Element(index=2, x=-2, y=-2, units="m"),
+            Element(index=3, x=2, y=2, units="m"),
+            Element(index=4, x=14, y=14, units="m")
+        ],
+        frequency=1e6,
+        units="m"
+    )
 
 
 @pytest.fixture()
@@ -23,26 +40,48 @@ def example_solution() -> Solution:
         apodizations=np.array([0.5, 0.75, 1.0, 0.85]),
         pulse=Pulse(frequency=42),
         sequence=Sequence(pulse_count=27),
-        focus=Point(id = "test_focus_point"),
-        target=Point(id = "test_target_point"),
-        simulation_result=xarray.Dataset(
-            {'pnp': (['x', 'y', 'z'], rng.random((3, 2, 3)))},
-            coords={'x': np.linspace(0, 1, 3), 'y': np.linspace(0, 1, 2), 'z': np.linspace(0, 1, 3)},
+        focus=Point(id="test_focus_point"),
+        target=Point(id="test_target_point"),
+        simulation_result=xa.Dataset(
+            {
+                'p_min': xa.DataArray(
+                    data=rng.random((3, 2, 3)),
+                    dims=["x", "y", "z"],
+                    attrs={'units': "Pa"}
+                ),
+                'p_max': xa.DataArray(
+                    data=rng.random((3, 2, 3)),
+                    dims=["x", "y", "z"],
+                    attrs={'units': "Pa"}
+                ),
+                'ita': xa.DataArray(
+                    data=rng.random((3, 2, 3)),
+                    dims=["x", "y", "z"],
+                    attrs={'units': "W/cm^2"}
+                )
+            },
+            coords={
+                'x': xa.DataArray(dims=["x"], data=np.linspace(0, 1, 3), attrs={'units': "m"}),
+                'y': xa.DataArray(dims=["y"], data=np.linspace(0, 1, 2), attrs={'units': "m"}),
+                'z': xa.DataArray(dims=["z"], data=np.linspace(0, 1, 3), attrs={'units': "m"})
+            }
         ),
     )
+
 
 def test_default_solution():
     """Ensure it is possible to construct a default Solution"""
     Solution()
 
+
 @pytest.mark.parametrize("compact_representation", [True, False])
 @pytest.mark.parametrize("include_simulation_data", [True, False])
 @pytest.mark.parametrize("default_solution", [True, False])
 def test_json_serialize_deserialize_solution(
-    example_solution:Solution,
-    compact_representation:bool,
-    include_simulation_data:bool,
-    default_solution:bool
+    example_solution: Solution,
+    compact_representation: bool,
+    include_simulation_data: bool,
+    default_solution: bool
 ):
     """Verify that turning a Solution into json and then re-constructing it gets back to the original solution"""
 
@@ -55,15 +94,22 @@ def test_json_serialize_deserialize_solution(
         solution_reconstructed = Solution.from_json(solution_json, simulation_result=solution.simulation_result)
     assert dataclasses_are_equal(solution_reconstructed, solution)
 
-def test_save_load_solution(example_solution:Solution, tmp_path:Path):
+
+def test_save_load_solution(example_solution: Solution, tmp_path: Path):
     """Test that a solution can be saved to and loaded from disk faithfully."""
     json_filepath = tmp_path/"some_directory"/"example_soln.json"
     example_solution.to_files(json_filepath)
     assert dataclasses_are_equal(Solution.from_files(json_filepath), example_solution)
 
-def test_save_load_solution_custom_dataset_filepath(example_solution:Solution, tmp_path:Path):
+
+def test_save_load_solution_custom_dataset_filepath(example_solution: Solution, tmp_path: Path):
     """Test that a solution can be saved to and loaded from disk faithfully, this time with a custom path for simulation data."""
     json_filepath = tmp_path/"some_directory"/"example_soln.json"
     nc_filepath = tmp_path/"some_other_directory"/"sim_output.nc"
     example_solution.to_files(json_filepath, nc_filepath)
     assert dataclasses_are_equal(Solution.from_files(json_filepath, nc_filepath), example_solution)
+
+
+def test_solution_analysis(example_solution: Solution, example_transducer: Transducer):
+    """Test that a solution output can be analyzed."""
+    example_solution.analyze(example_transducer)
