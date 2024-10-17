b514826a12278a7f971544ee058a144bd4518478
Loic Tetrel <loic.tetrel@kitware.com>
2024-10-17 17:43:57 +0200

    Removed `get_focus_matrix` because it is already implemented in geo.Point

--- src/openlifu/bf/__init__.py
+++ src/openlifu/bf/__init__.py
@@ -3,7 +3,6 @@ from .calc_dist_from_focus import calc_dist_from_focus
 from .delay_methods import DelayMethod
 from .focal_patterns import FocalPattern, SinglePoint, Wheel
 from .get_beamwidth import get_beamwidth
-from .get_focus_matrix import get_focus_matrix
 from .mask_focus import mask_focus
 from .offset_grid import offset_grid
 from .pulse import Pulse
@@ -17,7 +16,6 @@ __all__ = [
     "SinglePoint",
     "Pulse",
     "Sequence",
-    "get_focus_matrix",
     "offset_grid",
     "calc_dist_from_focus",
     "mask_focus",
