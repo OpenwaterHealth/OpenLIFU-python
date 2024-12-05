28d35d4ffacabf6a65e34c16e0386b6fbc9e69e2
Peter Hollender <peterhollender@gmail.com>
2024-12-05 11:20:58 -0500

    update masking unit test

--- tests/test_mask_focus.py
+++ tests/test_mask_focus.py
@@ -11,7 +11,7 @@ def example_focus() -> DataArray:
     return Point(
         id="test_focus_point",
         radius=1.,
-        position=np.array([0.5, 0.5, 0.5]),
+        position=np.array([1.0, 0.0, 1.5]),
         dims=("x", "y", "z"),
         units="mm"
     )
@@ -22,29 +22,32 @@ def example_xarr() -> DataArray:
     return Dataset(
             {
                 'p': DataArray(
-                    data=rng.random((3, 2, 3)),
+                    data=rng.random((3, 3, 5)),
                     dims=["x", "y", "z"],
                     attrs={'units': "Pa"}
                 )
             },
             coords={
-                'x': DataArray(dims=["x"], data=np.linspace(0, 1, 3), attrs={'units': "mm"}),
-                'y': DataArray(dims=["y"], data=np.linspace(0, 1, 2), attrs={'units': "mm"}),
-                'z': DataArray(dims=["z"], data=np.linspace(0, 1, 3), attrs={'units': "mm"})
+                'x': DataArray(dims=["x"], data=np.linspace(-1, 1, 3), attrs={'units': "mm"}),
+                'y': DataArray(dims=["y"], data=np.linspace(-1, 1, 3), attrs={'units': "mm"}),
+                'z': DataArray(dims=["z"], data=np.linspace(0, 2, 5), attrs={'units': "mm"})
             }
         )
 
 def test_mask_focus(example_xarr: Dataset, example_focus: Point):
     """Test that the distance grid from the focus point is correct."""
     expected = np.array(
-        [[[ True, True, True],
-          [ True, True, False]],
+        [[[False, False, False, False, False],
+        [False, False, False, False, False],
+        [False, False, False, False, False]],
 
-         [[ True, True, True],
-          [True, False, False]],
+       [[False, False, False, False, False],
+        [ True,  True,  True,  True, False],
+        [False, False, False, False, False]],
 
-         [[ True, True, False],
-          [ False, False, False]]])
-    mask = mask_focus(example_xarr, example_focus, distance=0.01)
+       [[False, False,  True,  True,  True],
+        [ True,  True,  True,  True,  True],
+        [False, False,  True,  True,  True]]])
+    mask = mask_focus(example_xarr, example_focus, distance=0.0011, aspect_ratio=(1.0, 1.0, 2.0))
 
     np.testing.assert_equal(mask, expected)
