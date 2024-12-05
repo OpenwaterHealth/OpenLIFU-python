Fix focus masking for solution analysis
Found one of the sources of regression in #141.  Focal masking was both using offset grid incorrectly (it was calculating the matrix centered at the origin in stead of the point because the default center behavior in `mask_focus` got switched between matlab and python. And the aspect ratio was being applied backwards in `mask_focus`, as a multiplication instead of a division, causing a pancake-shaped ROI instead of a cigar shaped ROI.

src/openlifu/bf/mask_focus.py
@@ -52,7 +52,7 @@ def mask_focus(
     # e.g. d = sqrt(((x'-x0')/ax)^2 + ((y'-y0')/ay)^2 + ((z'-z0')/az)^2). This is useful for calculating how far
     # away from an oblong focal spot each point is.
     ogrid = offset_grid(data_arr_rescaled, focus)
-    ogrid_aspect_corrected = ogrid*aspect_ratio
+    ogrid_aspect_corrected = ogrid/aspect_ratio
     m = np.sqrt(np.sum(ogrid_aspect_corrected**2, axis=-1))
 
     # mask based on distance