736b18ac284492b5e156b62ecaa41940d2e41d25
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2025-01-31 15:40:42 -0500

    Add spherical interpolator tool
    This is a follow-up to the work for #149,
    mainly to support the virtual fitting port #147.

--- tests/test_skinseg.py
+++ tests/test_skinseg.py
@@ -2,11 +2,16 @@ from typing import Tuple
 
 import numpy as np
 import pytest
+import vtk
 from scipy.linalg import expm
+from scipy.stats import skew
 
 from openlifu.seg.skinseg import (
+    cartesian_to_spherical,
     compute_foreground_mask,
     create_closed_surface_from_labelmap,
+    spherical_interpolator_from_mesh,
+    spherical_to_cartesian,
     take_largest_connected_component,
     vtk_img_from_array_and_affine,
 )
@@ -81,3 +86,116 @@ def test_create_closed_surface_from_labelmap():
         point_position = np.array(points.GetPoint(i))
         point_distance_from_sphere_center = np.linalg.norm(point_position - sphere_center, ord=2)
         assert np.abs(point_distance_from_sphere_center - sphere_radius) < 1.
+
+def test_spherical_coordinate_range():
+    """Verify that spherical coordinate output is in the prescribed value ranges"""
+    rng = np.random.default_rng(848)
+    # try all 8 octants of 3D space
+    for sign_x in [-1,1]:
+        for sign_y in [-1,1]:
+            for sign_z in [-1,1]:
+                cartesian_coords = np.array([sign_x, sign_y, sign_z]) * rng.random(size=3)
+                r, th, ph = cartesian_to_spherical(*cartesian_coords)
+                assert r>=0
+                assert 0 <= th <= np.pi
+                assert -np.pi <= ph <= np.pi
+
+def test_spherical_coordinate_conversion_inverse():
+    """Verify that the spherical coordinate conversion forward and backward functions are inverses of one another"""
+    rng = np.random.default_rng(241)
+    # try all 8 octants of 3D space
+    for sign_x in [-1,1]:
+        for sign_y in [-1,1]:
+            for sign_z in [-1,1]:
+                cartesian_coords = np.array([sign_x, sign_y, sign_z]) * rng.random(size=3)
+                np.testing.assert_almost_equal(
+                    spherical_to_cartesian(*cartesian_to_spherical(*cartesian_coords)),
+                    cartesian_coords
+                )
+                np.testing.assert_almost_equal(
+                    cartesian_to_spherical(*spherical_to_cartesian(*cartesian_to_spherical(*cartesian_coords))),
+                    cartesian_to_spherical(*cartesian_coords)
+                )
+
+def test_spherical_interpolator_from_mesh():
+    """Check using a torus that the spherical interpolator behaves reasonably"""
+    parametric_torus = vtk.vtkParametricTorus()
+    parametric_torus.SetRingRadius(12.)
+    parametric_torus.SetCrossSectionRadius(5)
+    parametric_function_source = vtk.vtkParametricFunctionSource()
+    parametric_function_source.SetUResolution(50)
+    parametric_function_source.SetVResolution(50)
+    parametric_function_source.SetParametricFunction(parametric_torus)
+    parametric_function_source.Update()
+    torus_polydata = parametric_function_source.GetOutput()
+
+    origin = (12., 0., 0.) # center at a point inside the torus, on the central ring of the torus
+    rng = np.random.default_rng(241)
+    xyz_direction_columns = expm((lambda A: (A - A.T)/2)(rng.normal(size=(3,3)))) # generate a random orthogonal matrix
+
+    interpolator = spherical_interpolator_from_mesh(
+        surface_mesh = torus_polydata,
+        origin = origin,
+        xyz_direction_columns = xyz_direction_columns,
+    )
+
+    sphere_source = vtk.vtkSphereSource()
+    sphere_source.SetRadius(1.0)
+    sphere_source.SetThetaResolution(50)  # Set the resolution in the theta direction
+    sphere_source.SetPhiResolution(50)  # Set the resolution in the phi direction
+    sphere_source.Update()
+    sphere_polydata = sphere_source.GetOutput()
+    sphere_points = sphere_polydata.GetPoints()
+    for i in range(sphere_points.GetNumberOfPoints()):
+        point = np.array(sphere_points.GetPoint(i))
+        r, theta, phi = cartesian_to_spherical(*point)
+        r = interpolator(theta, phi)
+        sphere_points.SetPoint(i, r * point)
+
+    xyz_affine = np.eye(4)
+    xyz_affine[:3,:3] = xyz_direction_columns
+    xyz_affine[:3,3] = origin
+    xyz_affine_vtkmat = vtk.vtkMatrix4x4()
+    xyz_affine_vtkmat.DeepCopy(xyz_affine.ravel())
+    xyz_transform = vtk.vtkTransform()
+    xyz_transform.SetMatrix(xyz_affine_vtkmat)
+    transform_filter = vtk.vtkTransformPolyDataFilter()
+    transform_filter.SetTransform(xyz_transform)
+    transform_filter.SetInputData(sphere_polydata)
+    transform_filter.Update()
+    sphere_polydata_transformed = transform_filter.GetOutput()
+
+    # These tools will be used to measure the disance between a point and a mesh
+    distance_from_torus = vtk.vtkImplicitPolyDataDistance()
+    distance_from_torus.SetInput(torus_polydata)
+    distance_from_sphere = vtk.vtkImplicitPolyDataDistance()
+    distance_from_sphere.SetInput(sphere_polydata_transformed)
+
+    # Now here is the unit test: sphere_polydata_transformed should be a sphere that has been "wrapped" around the torus.
+    # To test this we first check that (a) the points on sphere_polydata_transformed are generally close to the torus.
+    # Then we check that (b) the points of the torus are generally inside the sphere_polydata_transformed surface.
+    # We say "generally" and allow for a tolerance, because
+    # (a) some points on sphere_polydata_transformed will be far from the torus, for example as the sphere stretches over the donut hole
+    # (b) some points on the torus will slightly stick out of the sphere just because of the interpolation
+    # (the torus would be perfectly inside the sphere if the sphere mesh had infinite resolution, but it doesn't)
+    # This is why some tolerance is allowed in both checks.
+
+    sphere_transformed_points = sphere_polydata_transformed.GetPoints()
+    distances = [
+        np.abs(distance_from_torus.FunctionValue(sphere_transformed_points.GetPoint(i)))
+        for i in range(sphere_transformed_points.GetNumberOfPoints())
+    ]
+    assert np.quantile(distances,0.9) < 0.3 # The points on sphere_polydata_transformed are generally close to the torus.
+
+    torus_points = torus_polydata.GetPoints()
+    signed_distances = [
+        distance_from_sphere.FunctionValue(torus_points.GetPoint(i))
+        for i in range(torus_points.GetNumberOfPoints())
+    ]
+
+    # A point is *inside* iff the signed distance is negative, so ideally we should check that all signed_distance elements are negative.
+    # However we must leave a tolerance.
+    assert np.max(signed_distances) < 2 # With this tolerance, all torus points are inside rather than outside
+    assert skew(signed_distances) < 0 # The distribution of signed distances should be negatively skewed,
+    # the idea is that while most torus points are close to the sphere (signed distance hovering around 0)
+    # there is a tail in the distribution of signed distances consisting of points that are well inside the sphere
