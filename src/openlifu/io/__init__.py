ad7a94a43857a57146ab36fcf4b3174717aade39
George Vigelette <george@openwater.cc>
2025-02-13 15:46:51 -0500

    removed i2c

--- src/openlifu/io/__init__.py
+++ src/openlifu/io/__init__.py
@@ -1,6 +1,4 @@
 from openlifu.io.config import *  # noqa: F403
-from openlifu.io.i2c_data_packet import *  # noqa: F403
-from openlifu.io.i2c_status_packet import *  # noqa: F403
 from openlifu.io.LIFUInterface import LIFUInterface
 from openlifu.io.LIFUUart import *  # noqa: F403
 from openlifu.io.tx7332_if import *  # noqa: F403
