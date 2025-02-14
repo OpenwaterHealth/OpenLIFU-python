28015f2eec99da8f310784ab508f2e5a146056bd
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2025-02-13 22:17:17 -0500

    remove extraneous comments and imports

--- src/openlifu/io/__init__.py
+++ src/openlifu/io/__init__.py
@@ -1,14 +1,5 @@
-from openlifu.io.config import *  # noqa: F403
 from openlifu.io.LIFUInterface import LIFUInterface
-from openlifu.io.LIFUUart import *  # noqa: F403
-from openlifu.io.tx7332_if import *  # noqa: F403
-from openlifu.io.utils import *  # noqa: F403
 
 __all__ = [
-    # "PulseProfile",
-    # "DelayProfile",
-    # "Tx7332Registers",
-    # "print_regs",
-    # "swap_byte_order",
     "LIFUInterface",
 ]
