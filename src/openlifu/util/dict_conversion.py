0abd900d022369fe928b02ea8cbe2d5db85ffd69
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2025-03-20 10:47:59 -0400

    Build array conversion into DictMixin (#119)
    This is just a convenience so when we add new numpy array fields to
    dataclasses we don't have to manually build the numpy array conversion
    into the `from_dict` of the class

--- src/openlifu/util/dict_conversion.py
+++ src/openlifu/util/dict_conversion.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
-from dataclasses import asdict, dataclass
-from typing import Any, Dict, Type, TypeVar
+from dataclasses import asdict, dataclass, fields
+from typing import Any, Dict, Type, TypeVar, get_origin
+
+import numpy as np
 
 T = TypeVar('T', bound='DictMixin')
 
 @dataclass
 class DictMixin:
-    """Mixin for basic conversion to and from dict."""
+    """Mixin for basic conversion of a dataclass to and from dict."""
     def to_dict(self) -> Dict[str,Any]:
         """
         Convert the object to a dictionary
@@ -27,4 +29,12 @@ class DictMixin:
         """
         if "class" in parameter_dict:
             parameter_dict.pop("class")
-        return cls(**parameter_dict)
+        new_object = cls(**parameter_dict)
+
+        # Convert anything that should be a numpy array to numpy
+        for field in fields(cls):
+            # Note that sometimes "field.type" is a string rather than a type due to the "from annotations import __future__" stuff
+            if get_origin(field.type) is np.ndarray or field.type is np.ndarray or "np.ndarray" in field.type:
+                setattr(new_object, field.name, np.array(getattr(new_object,field.name)))
+
+        return new_object
