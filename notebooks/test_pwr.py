8e29de332959d65a803ffe271a609fec5b96c366
George Vigelette <george@openwater.cc>
2025-03-24 09:52:28 -0400

    update to power test script

--- notebooks/test_pwr.py
+++ notebooks/test_pwr.py
@@ -21,23 +21,23 @@ print("Ping the device")
 interface.hvcontroller.ping()
 
 print("Starting DAC value increments...")
+dac_value = 2800
+reg_value = 3800
+print(f"Setting DACs: hvp={dac_value}, hrp={reg_value}, hvm={dac_value}, hrm={reg_value}")
+if interface.hvcontroller.set_dacs(hvp=dac_value, hrp=reg_value, hvm=dac_value, hrm=reg_value):
+    print(f"DACs successfully set to hvp={dac_value}, hrp={reg_value}, hvm={dac_value}, hrm={reg_value}")
+else:
+    print("Failed to set DACs.")
 
-# Increment DAC values from 0 to 3000 by 100, with reg +400 capped at 4095
-for dac_value in range(0, 3001, 100):
-    reg_value = min(dac_value + 400, 4095)
-    print(f"Setting DACs: hvp={dac_value}, hrp={reg_value}, hvm={dac_value}, hrm={reg_value}")
-    if interface.hvcontroller.set_dacs(hvp=dac_value, hrp=reg_value, hvm=dac_value, hrm=reg_value):
-        print(f"DACs successfully set to hvp={dac_value}, hrp={reg_value}, hvm={dac_value}, hrm={reg_value}")
+print("Test HV Supply...")
+print("HV OFF. Press enter to TURN ON:")
+input()  # Wait for user input
+if interface.hvcontroller.turn_hv_on():
+    print("HV ON. Press enter to TURN OFF:")
+    input()  # Wait for user input
+    if interface.hvcontroller.turn_hv_off():
+        print("HV OFF.")
     else:
-        print("Failed to set DACs.")
-
-    print("Test HV Supply...")
-    if interface.hvcontroller.turn_hv_on():
-        print("HV ON. Press enter to TURN OFF:")
-        input()  # Wait for user input
-        if interface.hvcontroller.turn_hv_off():
-            print("HV OFF.")
-        else:
-            print("Failed to turn off HV.")
-    else:
-        print("Failed to turn on HV.")
+        print("Failed to turn off HV.")
+else:
+    print("Failed to turn on HV.")
