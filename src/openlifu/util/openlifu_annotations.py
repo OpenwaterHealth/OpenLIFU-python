e143107aa1ce7925512b4937fc9a8e3180d339f5
Andrew Howe <arhowe00@gmail.com>
2025-04-03 09:58:20 -0400

    Change OpenLIFUFieldData from dataclass to named tuple (#262)
    Co-authored-by: Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>

--- src/openlifu/util/openlifu_annotations.py
+++ src/openlifu/util/openlifu_annotations.py
@@ -1,18 +1,15 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
-from typing import Annotated
+from typing import Annotated, NamedTuple
 
 
-@dataclass
-class OpenLIFUFieldData:
+class OpenLIFUFieldData(NamedTuple):
     """
-    A lightweight class representing a name and annotation for the fields of a
-    dataclass. For instance, the Graph dataclass will have two fields that are
-    now associated with an OpenLIFUFieldData class containing its meaning:
+    A lightweight named tuple representing a name and annotation for the fields
+    of a dataclass. For example, the Graph dataclass may have fields associated
+    with this type:
 
     ```python
-    @dataclass
     class Graph:
         units: Annotated[str, OpenLIFUFieldData("Units", "The units of the graph")] = "mm"
         dim_names: Annotated[
@@ -21,9 +18,8 @@ class OpenLIFUFieldData:
         ] = ("lat", "ele", "ax")
     ```
 
-    Using Annotated[] will *not* interfere with runtime behavior or type
-    compatibility, and so fields will behave as though no annotation is present.
+    Annotated[] does not interfere with runtime behavior or type compatibility.
     """
 
-    name : Annotated[str | None, "The name of the dataclass field."] = field(default=None)
-    description : Annotated[str | None, "The description of the dataclass field."] = field(default=None)
+    name: Annotated[str | None, "The name of the dataclass field."]
+    description: Annotated[str | None, "The description of the dataclass field."]
