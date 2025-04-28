d294769f594eed2622e12fe23bf1c4aded898f6c
Peter Hollender <peterhollender@gmail.com>
2025-04-28 11:22:28 -0400

    Fix ABC, add tests (#290)

--- tests/test_seg_method.py
+++ tests/test_seg_method.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 import pytest
 
-from openlifu.seg import Material, SegmentationMethod, seg_methods
+from openlifu.seg import MATERIALS, Material, SegmentationMethod, seg_methods
+from openlifu.seg.seg_methods.uniform import UniformSegmentation
 
 
 @pytest.fixture()
@@ -31,3 +32,61 @@ def example_seg_method() -> seg_methods.UniformSegmentation:
 
 def test_seg_method_dict_conversion(example_seg_method : seg_methods.UniformSegmentation):
     assert SegmentationMethod.from_dict(example_seg_method.to_dict()) == example_seg_method
+
+def test_seg_method_no_instantiate_abstract_class():
+    with pytest.raises(TypeError):
+        SegmentationMethod(
+            materials = {
+                'water' : Material(
+                    name="water",
+                    sound_speed=1500.0,
+                    density=1000.0,
+                    attenuation=0.0,
+                    specific_heat=4182.0,
+                    thermal_conductivity=0.598
+                ),
+                'skull' : Material(
+                    name="skull",
+                    sound_speed=4080.0,
+                    density=1900.0,
+                    attenuation=0.0,
+                    specific_heat=1100.0,
+                    thermal_conductivity=0.3
+                ),
+            },
+            ref_material = 'water',
+        )
+
+def test_uniform_seg_method_no_reference_material():
+    with pytest.raises(ValueError, match="Reference material non_existent_material not found."):
+        UniformSegmentation(
+            materials = {
+                'water' : Material(
+                    name="water",
+                    sound_speed=1500.0,
+                    density=1000.0,
+                    attenuation=0.0,
+                    specific_heat=4182.0,
+                    thermal_conductivity=0.598
+                ),
+                'skull' : Material(
+                    name="skull",
+                    sound_speed=4080.0,
+                    density=1900.0,
+                    attenuation=0.0,
+                    specific_heat=1100.0,
+                    thermal_conductivity=0.3
+                ),
+            },
+            ref_material = 'non_existent_material',
+        )
+
+def test_uniformwater_errors_when_specify_ref_material():
+    with pytest.raises(TypeError):
+        seg_methods.UniformWater(
+            ref_material = 'water',
+        )
+
+def test_materials_as_none_gets_default_materials():
+    seg_method = seg_methods.UniformSegmentation(materials=None)
+    assert seg_method.materials == MATERIALS.copy()
