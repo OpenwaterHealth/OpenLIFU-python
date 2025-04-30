eb2aae1d1b5a98b132ab15144ffe36d1e894b1e2
Andrew Howe <arhowe00@gmail.com>
2025-04-30 10:19:29 -0400

    Update noxfile to (in some cases) use requirements.txt

--- noxfile.py
+++ noxfile.py
@@ -41,6 +41,7 @@ def tests(session: nox.Session) -> None:
     Run the unit and regular tests.
     """
     session.install(".[test]")
+    session.install("-r", "requirements.txt")
     session.run("pytest", *session.posargs)
 
 
