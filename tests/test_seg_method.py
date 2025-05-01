83fff00c957153c2cc27ee2233ce4bfc5989c85d
Andrew Howe <arhowe00@gmail.com>
2025-05-01 17:42:29 -0400

    Correct `seg_method` tests (#290)
    - Simplify and explicitly ignore pyright errors in seg_method tests
      meant to protect against accidental call errors and misuse.

--- tests/test_seg_method.py
+++ tests/test_seg_method.py
@@ -35,27 +35,7 @@ def test_seg_method_dict_conversion(example_seg_method : seg_methods.UniformSegm
 
 def test_seg_method_no_instantiate_abstract_class():
     with pytest.raises(TypeError):
-        SegmentationMethod(
-            materials = {
-                'water' : Material(
-                    name="water",
-                    sound_speed=1500.0,
-                    density=1000.0,
-                    attenuation=0.0,
-                    specific_heat=4182.0,
-                    thermal_conductivity=0.598
-                ),
-                'skull' : Material(
-                    name="skull",
-                    sound_speed=4080.0,
-                    density=1900.0,
-                    attenuation=0.0,
-                    specific_heat=1100.0,
-                    thermal_conductivity=0.3
-                ),
-            },
-            ref_material = 'water',
-        )
+        SegmentationMethod()  # pyright: ignore[reportAbstractUsage]
 
 def test_uniform_seg_method_no_reference_material():
     with pytest.raises(ValueError, match="Reference material non_existent_material not found."):
@@ -84,9 +64,9 @@ def test_uniform_seg_method_no_reference_material():
 def test_uniformwater_errors_when_specify_ref_material():
     with pytest.raises(TypeError):
         seg_methods.UniformWater(
-            ref_material = 'water',
-        )
+                ref_material = 'water',  # pyright: ignore[reportCallIssue]
+            )
 
 def test_materials_as_none_gets_default_materials():
-    seg_method = seg_methods.UniformSegmentation(materials=None)
+    seg_method = seg_methods.UniformSegmentation(materials=None)  # pyright: ignore[reportArgumentType]
     assert seg_method.materials == MATERIALS.copy()
