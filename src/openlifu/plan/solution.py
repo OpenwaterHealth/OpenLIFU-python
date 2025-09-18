Fix output calculation #384

src/openlifu/plan/solution.py
@@ -189,12 +189,7 @@ class Solution:
             apodization = self.apodizations[focus_index]
             origin = self.transducer.get_effective_origin(apodizations=apodization, units=options.distance_units)
 
-            output_signal_Pa = []
-            output_signal_Pa = np.zeros((self.transducer.numelements(), len(input_signal_V)))
-            for i in range(self.transducer.numelements()):
-                apod_signal_V = input_signal_V * self.apodizations[focus_index, i]
-                output_signal_Pa[i] = self.transducer.elements[i].calc_output(apod_signal_V, dt)
-
+            output_signal_Pa = self.transducer.calc_output(input_signal_V, dt, delays=self.delays[focus_index, :], apod=self.apodizations[focus_index, :])
             p0_Pa = np.max(output_signal_Pa, axis=1)
 
             mainlobe_mask = get_mask(