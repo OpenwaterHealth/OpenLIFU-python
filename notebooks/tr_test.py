7034d740d58a0e73796ae650520da26f37cda79d
evjlind <evanjlindeman@gwu.edu>
2025-11-14 10:42:12 -0500

    obtained delays from TR sim and performed forward sim with obtained delays

--- notebooks/tr_test.py
+++ notebooks/tr_test.py
@@ -82,14 +82,14 @@ def generate_ct_noise(kgrid):
     return sine_image
 
 # set focus
-simulate = False
+simulate = True
 plot = True
-simulate2 = False
-use_ct_noise = True
+simulate2 = True
+use_ct_noise = False
 
 xInput = 0
 yInput = 0
-zInput = 25
+zInput = 35
 
 frequency_kHz = 400 # Frequency in kHz
 duration_msec = 0.1 # Pulse Duration in milliseconds
@@ -104,8 +104,7 @@ sequence = Sequence(
     pulse_train_interval=0,
     pulse_train_count=1)
 
-here = Path(__file__).parent.resolve()
-db_path = here / ".." / "OpenLIFU_Database_DCVA"
+db_path = "C:\\Users\\evanj\\OneDrive\\Desktop\\Work\\OpenLIFU-python-VA\\OpenLIFU_Database_DCVA\\"
 db = Database(db_path)
 arr = db.load_transducer(f"openlifu_{num_modules}x400_evt1_002")
 
@@ -198,41 +197,34 @@ if plot:
     ax.set_xlabel('x')
     ax.set_ylabel('y')
 
-x_values = np.floor(kgrid.Nx/2)
-y_values = np.floor(kgrid.Ny/2)
-z_values = np.floor(kgrid.Nz/2)
+x_values = np.floor(kgrid.Nx*spacing)
+y_values = np.floor(kgrid.Ny*spacing)
+z_values = np.floor(kgrid.Nz*spacing)
 
 x_range = np.arange(-x_values/2,x_values/2+spacing,spacing)
 y_range = np.arange(-y_values/2,y_values/2+spacing,spacing)
 z_range = np.arange(-z_values/2,z_values/2+spacing,spacing)
-ele_bin = np.zeros((kgrid.Nx,kgrid.Ny,kgrid.Nz))
+
+ele_bin = np.zeros([kgrid.Nx,kgrid.Ny,kgrid.Nz])
 ele_sensors = np.empty((128))
 
 for i in range(128):
     ind_x = find_nearest(x_range,sensor_mask_pos[0][i])
     ind_y = find_nearest(y_range,sensor_mask_pos[1][i])
     ind_z = find_nearest(z_range,sensor_mask_pos[2][i])
-    print(f'{(sensor_mask_pos[0][i],sensor_mask_pos[1][i],sensor_mask_pos[2][i])}:{(ind_x,ind_y,ind_z)}')
     ele_bin[ind_x,ind_y,ind_z] = 1
 
-# if plot:
-#     xs, ys, zs = (ele_sensors[0],ele_sensors[1],ele_sensors[2])
-#     fig = plt.figure(figsize=(12, 12))
-#     ax = fig.add_subplot(projection='3d')
-#     ax.set_box_aspect((np.ptp(xs), np.ptp(ys), np.ptp(zs))) 
-#     ax.scatter(ele_sensors[0],ele_sensors[1],ele_sensors[2])
-    
-#     ax.set_xlabel('x')
-#     ax.set_ylabel('y')
-
-# print(ele_sensors)
 
 if simulate2:
+    # p_max_np = p_max.to_numpy()
+    # p0 = np.zeros_like(p_max_np)
+    # p0[round(kgrid.Nx/2)-7:round(kgrid.Nx/2)+7,round(kgrid.Ny/2)-7:round(kgrid.Ny/2)+7,zInput-15:zInput+15] = p_max_np[round(kgrid.Nx/2)-7:round(kgrid.Nx/2)+7,round(kgrid.Ny/2)-7:round(kgrid.Ny/2)+7,zInput-15:zInput+15]
+    p0 = p_max.to_numpy()
     sensor2 = kSensor(record=['p'])
     sensor2.mask = ele_bin
     # sensor2.mask = karray.get_array_binary_mask(kgrid)
     source2 = kSource()
-    source2.p0 = p_max.to_numpy()
+    source2.p0 = p0
     kgrid2 = get_kgrid(coords)
     if use_ct_noise:
         ct_noise = generate_ct_noise(kgrid2)
@@ -242,7 +234,7 @@ if simulate2:
         #     plt.title('noisegen')
         #     plt.colorbar()
         model = np.zeros_like(ct_noise)
-        model[:,:,zInput-7:zInput-3] = ct_noise[:,:,zInput-7:zInput-3]
+        model[:,:,zInput-5:zInput-3] = ct_noise[:,:,zInput-5:zInput-3]
         dmap = rho_min + (rho_max-rho_min)*(model-0)/(hu_max-0)
         cmap = c_min + (c_max-c_min)*(dmap-rho_min)/(rho_max-rho_min)
         amap = alpha_coeff_min+(alpha_coeff_max-alpha_coeff_min)*(1-(model-hu_min)/(hu_max-hu_min))**0.5
@@ -268,46 +260,109 @@ if simulate2:
 
     if plot:
         plt.figure(figsize=(10, 6))
-        plt.imshow(sensor_data['p'], aspect='auto', extent=[
+        plt.imshow(sensor_data['p'].T, aspect='auto', extent=[
             0, kgrid.Nt * kgrid.dt * 1e6,  # Time in μs
-            0, sensor_data['p'].shape[0]  # Sensor number
+            0, sensor_data['p'].shape[1]  # Sensor number
         ])
         plt.xlabel('Time (μs)')
         plt.ylabel('Sensor Number')
         plt.title('Recorded Pressure at Boundary Sensors')
         plt.colorbar(label='Pressure (Pa)')
 
-    medium3 = get_medium(params)
-    sensor3 = kSensor(record=['p'])
-    sensor3.mask = ele_bin
-    # sensor2.mask = karray.get_array_binary_mask(kgrid)
-    source3 = kSource()
-    source3.p0 = p_max.to_numpy()
-    kgrid3 = get_kgrid(coords)
-    sensor_data = kspaceFirstOrder3D(
-        kgrid=kgrid3,
-        source=source3,
-        sensor=sensor3,
-        medium=medium3,
-        simulation_options=simulation_options,
-        execution_options=execution_options
-    )
+delays_tr = np.zeros_like(delays)
+for i in range(128):
+    # print(f'element #{i}')
+    if use_ct_noise:
+        amp, phase, freq = extract_amp_phase(np.squeeze(sensor_data['p'].T[i]),1/kgrid.dt,freq,dim=0)
+    else:
+        amp, phase, freq = extract_amp_phase(np.squeeze(sensor_data['p'].T[i]),1/kgrid.dt,freq,dim=0)
+    delays_tr[i]=phase/freq/(2*np.pi)
 
-    if plot:
-        plt.figure(figsize=(10, 6))
-        plt.imshow(sensor_data['p'], aspect='auto', extent=[
-            0, kgrid.Nt * kgrid.dt * 1e6,  # Time in μs
-            0, sensor_data['p'].shape[0]  # Sensor number
-        ])
-        plt.xlabel('Time (μs)')
-        plt.ylabel('Sensor Number')
-        plt.title('Recorded Pressure at Boundary Sensors no noise')
-        plt.colorbar(label='Pressure (Pa)')
+delays_tr = delays_tr+abs(min(delays_tr))
+print(delays_tr)
+
+# print(phase)
+# plt.figure()
+# plt.plot(phase)
+# plt.show()
+
+source_mat = arr.calc_output(input_signal, kgrid.dt, delays_tr, apod)
+karray = get_karray(arr,
+                    translation=array_offset,
+                    bli_tolerance=bli_tolerance,
+                    upsampling_rate=upsampling_rate)
+
+medium = get_medium(params)
+sensor = get_sensor(kgrid, record=['p_max', 'p_min'])
+source = get_source(kgrid, karray, source_mat)
+medium2 = kWaveMedium(sound_speed=cmap,density=dmap,alpha_coeff=amap,alpha_power=alpha_power,alpha_mode='no_dispersion')
+
+if simulate:
+    output = kspaceFirstOrder3D(kgrid=kgrid,
+                                    source=source,
+                                    sensor=sensor,
+                                    medium=medium,
+                                    simulation_options=simulation_options,
+                                    execution_options=execution_options)
+
+    sz = list(params.coords.sizes.values())
+    p_max = xa.DataArray(output['p_max'].reshape(sz, order='F'),
+                            coords=params.coords,
+                            name='p_max',
+                            attrs={'units':'Pa', 'long_name':'PPP'})
+    p_min = xa.DataArray(-1*output['p_min'].reshape(sz, order='F'),
+                            coords=params.coords,
+                            name='p_min',
+                            attrs={'units':'Pa', 'long_name':'PNP'})
+    Z = params['density'].data*params['sound_speed'].data
+    intensity = xa.DataArray(1e-4*output['p_min'].reshape(sz, order='F')**2/(2*Z),
+                            coords=params.coords,
+                            name='I',
+                            attrs={'units':'W/cm^2', 'long_name':'Intensity'})
+    ds = xa.Dataset({'p_max':p_max, 'p_min':p_min, 'intensity':intensity})
+    if plot == True:
+        plt.figure()
+        plt.imshow(p_max[:,round(kgrid.Ny/2),:])
+        plt.title('forward sim with TR delays')
+        plt.colorbar()
+        
+source_mat = arr.calc_output(input_signal, kgrid.dt, delays_tr, apod)
+karray = get_karray(arr,
+                    translation=array_offset,
+                    bli_tolerance=bli_tolerance,
+                    upsampling_rate=upsampling_rate)
+
+sensor = get_sensor(kgrid, record=['p_max', 'p_min'])
+source = get_source(kgrid, karray, source_mat)
+
+if simulate and use_ct_noise:
+    output = kspaceFirstOrder3D(kgrid=kgrid,
+                                    source=source,
+                                    sensor=sensor,
+                                    medium=medium2,
+                                    simulation_options=simulation_options,
+                                    execution_options=execution_options)
+
+    sz = list(params.coords.sizes.values())
+    p_max = xa.DataArray(output['p_max'].reshape(sz, order='F'),
+                            coords=params.coords,
+                            name='p_max',
+                            attrs={'units':'Pa', 'long_name':'PPP'})
+    p_min = xa.DataArray(-1*output['p_min'].reshape(sz, order='F'),
+                            coords=params.coords,
+                            name='p_min',
+                            attrs={'units':'Pa', 'long_name':'PNP'})
+    Z = params['density'].data*params['sound_speed'].data
+    intensity = xa.DataArray(1e-4*output['p_min'].reshape(sz, order='F')**2/(2*Z),
+                            coords=params.coords,
+                            name='I',
+                            attrs={'units':'W/cm^2', 'long_name':'Intensity'})
+    ds = xa.Dataset({'p_max':p_max, 'p_min':p_min, 'intensity':intensity})
+    if plot == True:
+        plt.figure()
+        plt.imshow(p_max[:,round(kgrid.Ny/2),:])
+        plt.title('forward sim with TR delays with ct noise')
+        plt.colorbar()
 
 if plot:
     plt.show()
-
-print('num ele')
-print(np.sum(ele_bin))
-print(f'num grid: {np.sum(sensor2.mask)}')
-# amp, phase, freq = extract_amp_phase(np.squeeze(sensor_data['p']),1/kgrid.dt,freq)
