27392f0aef5e43be63029a72af9741de4fb1c1d4
Alex <akagan@openwater.cc>
2026-01-23 17:23:29 -0800

    Override voltage table temporarily

--- src/openlifu/io/LIFUInterface.py
+++ src/openlifu/io/LIFUInterface.py
@@ -17,10 +17,10 @@ from openlifu.plan.solution import Solution
 
 REF_MAX_SEQUENCE_TIMES = {
     "default": [2*60, 5*60, 10*60],    # users to use default values
-    "stress_test": [5*60, 9*60, 15*60] # QA to use stress test values
+    "stress_test": [15*60, 15*60, 15*60] # QA to use stress test values
 }
 
-REF_MAX_DUTY_CYCLES = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5]
+REF_MAX_DUTY_CYCLES = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
 
 MAX_VOLTAGE_BY_DUTY_CYCLE_AND_SEQUENCE_TIME = {
     "evt2": [
